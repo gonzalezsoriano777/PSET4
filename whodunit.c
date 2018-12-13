f822de799836b501dfacf9aa64453ea8e0956e2a
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-13 18:38:02 +0000

    Work

---
+++ whodunit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+#include "bmp.h"
+
+
+
+int main(int argc, char* argv[])
+{
+    // printf("Hello, World\n");
+
+    // Implements 4 arguements
+    if (argc != 4)
+    {
+        printf("Usage: ./ Reminder correct Usage\n"); // Reminds the user of correct usage
+        return 1;
+    }
+
+
+
+}
