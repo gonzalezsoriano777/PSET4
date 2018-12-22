b2cb699c446b46921d55feba65a092d8fd20a626
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-22 01:36:55 +0000

    Success, resizes the image based of n, compiles and prints in term and has same color

--- resize/resize.c
+++ resize/resize.c
@@ -1,4 +1,5 @@
-// Copies a BMP file .. for Resize of bitmap files
+// Copies a BMP file
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,20 +14,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // converts 'n' which is the resizer into an integer
-    int n = atoi(argv[1]);
     // remember filenames
+    int *n = malloc(sizeof(int));
+    *n = atoi(argv[1]);
     char *infile = argv[2];
     char *outfile = argv[3];
 
-    // resize must be positive and should be less then or equal to 100
-    if(n < 0 || n > 100)
+
+    if (*n < 0 || *n > 100)
     {
-        fprintf(stderr, "Resize must be in the range of [1 - 100]\n");
-        return 1;
+        fprintf(stderr, "Range must consist of [1- 100]\n");
+        return 5;
     }
-
-
 
     // open input file
     FILE *inptr = fopen(infile, "r");
@@ -49,34 +48,16 @@ int main(int argc, char *argv[])
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
-
     // read infile's BITMAPINFOHEADER
-    BITMAPINFOHEADER bi, Bih;
+    BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
-    // creates new infiles for BitmapInfo
-    // Used to rescale the width and height for the bitmap
 
 
-    // Times the width and height of the small.bmp based on the number put for n (this goes for both Width and Height)
-    // changes size of bitmap Horizontally and Vertically by n
-    Bih.biWidth = bi.biWidth * n;
-    Bih.biHeight = abs(bi.biHeight) * n;
-
-    printf("width: %i height: %i\n", Bih.biWidth, Bih.biHeight);
-
-    int outPadding = (4 - (Bih.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
-    //   Overall grabbing the Sizeimage (in pixels) using the RGBTRIPLE and adding the outfile of BitmapInfo and padding
-    //   Computing the absolute value of biHeight for the outfile
-    Bih.biSizeImage = ((sizeof(RGBTRIPLE) * Bih.biWidth) + outPadding) * abs(Bih.biHeight);
-    bf.bfSize = Bih.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-
-    printf("%i %i\n", Bih.biSizeImage, bf.bfSize);
 
 
-// ensure infile is (likely) a 24-bit uncompressed BMP 4.0
 
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
@@ -86,76 +67,90 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    BITMAPFILEHEADER Bfh;
+    Bfh = bf;
 
+    BITMAPINFOHEADER Bih;
+    Bih = bi;
+    // Horizontally(Width) and Vertically(Height) by n
+    Bih.biWidth = bi.biWidth * *n;
+    Bih.biHeight = (bi.biHeight * *n);
 
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
+    printf("Width: %i Height: %i\n", bi.biWidth, bi.biHeight);
 
     // determine padding for scanlines
+    int outPadding = (4 - (Bih.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
+    // Calculates the image the file relating to pixels
+    // Same process but for the file in bytes
+    Bih.biSizeImage = (Bih.biWidth * sizeof(RGBTRIPLE) + outPadding) * abs(Bih.biHeight);
+    printf("Resized Image: %i\n", Bih.biSizeImage);
 
+    // specific number of bytes the header takes up
+    Bfh.bfSize = Bih.biSizeImage + 54;
 
-    // temporary storage
-    RGBTRIPLE triple;
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&Bfh, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&Bih, sizeof(BITMAPINFOHEADER), 1, outptr);
+
 
 
     // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    for (int sLine = 0, biHeight = abs(bi.biHeight); sLine < biHeight; sLine++)
     {
-        // each row will be printed out n times
-        int rowcounter = 0;
 
-        while(rowcounter < n)
+
+        // iterate over rows in scanline
+        for (int rows = 0; rows < *n; rows++)
         {
-
-        // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
-        {
+            // printf("%i\n", rows);
 
 
-                // printf("%i\n", j);
+            // iterate over pixels in scanline
+            for (int pixels = 0; pixels < bi.biWidth; pixels++)
+            {
+                //  printf("%i\n", pixels);
 
-                // each pixel will be printed out n times
-                int pixelCounter = 0;
+                // temporary storage
+                RGBTRIPLE triple;
 
-               // read RGB triple from infile
-               fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-                while(pixelCounter < n)
+                for (int pixell = 0; pixell < *n; pixell++)
                 {
-                   // write RGB triple to outfile
-                   fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-                   pixelCounter++;
+                    // write RGB triple to outfile
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
                 }
+
+                printf("%i%i%i\n", triple.rgbtRed, triple.rgbtGreen, triple.rgbtBlue);
+            }
+
+            // skip over padding, if any
+            fseek(inptr, padding, SEEK_CUR);
+
+            // then add it back (to demonstrate how)
+            for (int k = 0; k < outPadding; k++)
+            {
+                fputc(0x00, outptr);
+            }
+
+            if (rows < *n - 1)
+            {
+                // send the infile cursor back, IF n is less an -1 as well as
+                // # of bytes for cursor to go back
+                long off = bi.biWidth * sizeof(RGBTRIPLE) + padding;
+                fseek(inptr, -off, SEEK_CUR);
+            }
         }
-
-
-        // add new padding
-        for (int k = 0; k < outPadding; k++)
-
-            fputc(0x00, outptr);
-
-            if(rowcounter < (n - 1))
-
-                 fseek(inptr, -(Bih.biWidth * sizeof(RGBTRIPLE)), SEEK_CUR);
-                 rowcounter++;
-
-
-        }
-
-    // skip over padding, if any
-    fseek(inptr, padding, SEEK_CUR);
-
     }
 
-
-
-    // Empty space to store memory in which is the value of the address of solution
-    // realloc(solution);
+    // allocating n
+    free(n);
 
     // close infile
     fclose(inptr);
